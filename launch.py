a794803990056805b8bbc29c451b2382ac13a46c
Andis Zeltins <andis.zeltins@prodrive-technologies.com>
2020-03-20 14:56:35 +0100

    asda

--- launch.py
+++ launch.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.get("http://www.sudoku.com")
@@ -9,5 +10,8 @@ driver.get("http://www.sudoku.com")
 wait = WebDriverWait(driver, 10)
 element = wait.until(EC.element_to_be_clickable((By.ID, 'game')))
 
-newgame = driver.find_element_by_class_name("button.new-game-button")
+newgame = driver.find_element_by_class_name("game-cell")
 newgame.click()
+
+one = driver.find_element_by_class_name("numpad-item")
+one.click()
