6d103512ceb740289e6f7b977070d3630fd7728d
melvinsxo <edviinsb@gmail.com>
2020-03-20 15:58:48 +0200

    launch and launch v1 fix

--- launch_v1.py
+++ launch_v1.py
@@ -1,15 +1,13 @@
 from selenium import webdriver
-#from selenium import webdriverwait
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
-driver.maximize_window()
 driver.get("http://www.sudoku.com")
 
 wait = WebDriverWait(driver, 10)
 element = wait.until(EC.element_to_be_clickable((By.ID, 'game')))
 
-#driver.implicitly_wait(10)
-firstcell = driver.find_element_by_xpath('/html/body/header/div/div[3]/div[1]')[0]
-firstcell.click()
-
+newgame = driver.find_element_by_xpath('//*[@id="masthead"]/div/div[3]/div[1]')
+newgame.click()
