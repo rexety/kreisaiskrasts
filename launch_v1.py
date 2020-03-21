b17ce1ceac9c765ce763baaaca4b9e4c655b3ae7
melvinsxo <edviinsb@gmail.com>
2020-03-21 14:49:59 +0200

    fix

--- launch_v1.py
+++ launch_v1.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import numpy as np
 import time
 
@@ -24,12 +25,14 @@ for x in f:
     if temp == 10:
         break
 
+#open browser & wait until loaded
 driver = webdriver.Chrome()
 driver.get("http://www.sudoku.com/easy/")
 
 wait = WebDriverWait(driver, 10)
 element = wait.until(EC.element_to_be_clickable((By.ID, 'game')))
 
+#read table into an ndarray
 for row in range(1,10):  
     for cell in range(1,10):     
         web_cells = driver.find_element_by_xpath(f'//*[@id="game"]/table/tbody/tr[{row}]/td[{cell}]')   
@@ -39,12 +42,23 @@ for row in range(1,10):
             
             for path in paths:
                 web_value = path.get_attribute("d")       
-                
+                    
             for keys, values in vectordict.items():
                 if values == web_value:
                     output[row-1,cell-1] = keys   
    
 print(output)
+
+##solve with function from the strategy file --- HOW IT SHOULD WORK
+#import strategy
+#solve(output) 
+
+
+#input
+newgame = driver.find_element_by_class_name("game-cell")
+newgame.click()
+newgame.send_keys(Keys.ARROW_DOWN)
+
 
 t1 = time.time()
 total = t1-t0
