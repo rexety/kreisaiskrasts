24bf10b92b8429c28f1038e9c58364bccef65476
rexety <andozelto@gmail.com>
2020-03-21 17:29:44 +0100

    new structure

--- launch.py
+++ launch.py
@@ -5,51 +5,62 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import numpy as np
 import time
-empty_base = np.zeros(shape=(9,9))
 
+t0 = time.time()
 
+f = open("vectorpaths.txt").read().splitlines()
+output = np.zeros(shape=(9,9))
+
+#cleaner dict solution possible?
 keys = []
 for i in range(1,10):
     keys.append(i)
-vectordic = dict.fromkeys(keys)
-
-f = open("vectorpaths.txt").read().splitlines()
+    
+vectordict = dict.fromkeys(keys)
 
 temp = 1
 for x in f:
-    vectordic[temp] = x
+    vectordict[temp] = x
     temp += 1
     if temp == 10:
         break
-    
-    
 
-driver = webdriver.Chrome()
+#open browser & wait until loaded
+driver = webdriver.Chrome(executable_path=r"C:\Python\chromedriver.exe")
 driver.get("http://www.sudoku.com/easy/")
 
 wait = WebDriverWait(driver, 10)
 element = wait.until(EC.element_to_be_clickable((By.ID, 'game')))
 
-newgame = driver.find_element_by_class_name("game-cell")
-newgame.click()
-newgame.send_keys(Keys.NUMPAD2)
 
-#through dynamic xpath iterate over whole 9x9 matrix, if empty put zero else read vector path
-
-for row in range(1,10):
-    for cell in range(1,10):
-        matrixcell = driver.find_element_by_xpath("//*[@id='game']/table/tbody/tr["+str(row)+"]/td["+str(cell)+"]")
-        cellvar = matrixcell.get_attribute("class")
-        if cellvar == "game-cell":
-            empty_base[row-1,cell-1] = 0
-        else:
-            paths = matrixcell.find_elements_by_css_selector("path")
+#read table into an ndarray
+for row in range(1,10):  
+    for cell in range(1,10):     
+        web_cells = driver.find_element_by_xpath(f'//*[@id="game"]/table/tbody/tr[{row}]/td[{cell}]')   
+        
+        if web_cells.get_attribute("class") == "game-cell game-value":
+            paths = web_cells.find_elements_by_tag_name("path")
+            
             for path in paths:
-                value = path.get_attribute("d") 
-            for keys, values in vectordic.items():
-                if values == value:
-                    empty_base[row-1,cell-1] = keys
-                    value = ""
+                web_value = path.get_attribute("d")       
+                    
+            for keys, values in vectordict.items():
+                if values == web_value:
+                    output[row-1,cell-1] = keys   
+   
+print(output)
 
-print(empty_base)
+##solve with function from the strategy file --- HOW IT SHOULD WORK
+#import strategy
+#solve(output) 
 
+
+# #input
+# newgame = driver.find_element_by_class_name("game-cell")
+# newgame.click()
+# newgame.send_keys(Keys.ARROW_DOWN)
+
+
+t1 = time.time()
+total = t1-t0
+print(total)
