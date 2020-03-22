464a4e63edf8b19f4ba045b2f16b31e0321c77df
rexety <andozelto@gmail.com>
2020-03-22 20:16:34 +0100

    zhopix

--- strategy.py
+++ strategy.py
@@ -1,16 +1,13 @@
 import numpy as np
 #from launch import output
-
+import sys
 # load given sudoku field and rename it to rows
-example = np.load('example.npy')
-rows = example
+rows = np.load('example.npy')
 
+previous = 1
 # manipulate given matrix so that columns are in rows
-columns = np.rot90(example)
+columns = np.rot90(rows)
 columns = np.flip(columns, 0)
-
-
-
 
 # outputs 3x3 sudoku fields in rows from given 9x9 sudoku matrix
 tempfield = np.array([])
@@ -20,27 +17,30 @@ for x in range(0,9,3):
      for i in range(1,4):
          temp = i * 3
          i = temp - 3
-         tempfield = np.append(tempfield,example[x:y, i:temp])
+         tempfield = np.append(tempfield,rows[x:y, i:temp])
                                                          
 fields = np.reshape(tempfield,(9,9))
 
 
 
-print(rows)
-print(columns)
-print(fields)
-
-
-
-"""
-we got 3 arrays - row, column, fields
-
-
-start row, if other than 0 use that cell
-when cell picked check if the row and columnd does
-not have a matching number
-
-"""
+def newmatrix(number,row,column):
+    
+    global rows
+    global columns
+    global fields
+    rows[row][column] = number
+    columns = np.rot90(rows)
+    columns = np.flip(columns, 0)
+    tempfield = np.array([])
+    temp = 0
+    for x in range(0,9,3):
+        y = x + 3
+        for i in range(1,4):
+            temp = i * 3
+            i = temp - 3
+            tempfield = np.append(tempfield,rows[x:y, i:temp])
+                                                         
+    fields = np.reshape(tempfield,(9,9))
 
 
 def rowcheck(number,row):
@@ -57,33 +57,97 @@ def columncheck(number,column):
     """
     return number in columns[column]
 
-def fieldcheck(number,field):
+def fieldcheck(number,row,column):
     """
     checks if given int is in the selected field
+
     """
-
-
-
-
-
-
-rowy = -1 # counts which row it is with an int
-coly = -1 # counts which column it is with an int
-for rowx in example:
-    rowy += 1
-    for cellx in rowx:
-        coly += 1
-        if cellx == 0:
-            for i in range(1,9):
-                if rowcheck(i,rowy) == True and columncheck(i,coly) == True: #checks if int can be used in the free space according to sudoku rules
-                    break#print("wat")
-                coly = -1
-               
+    sectormatrix = ([0,1,2],
+                    [3,4,5],
+                    [6,7,8])
+    
+    counter = -1
+    sectorcounter = -1
+    for i in range(3,10,3):
+        counter += 1
+        if row < i:
+            for j in range(3,10,3):
+                sectorcounter += 1
+                if column < j:
+                    return number in fields[sectormatrix[counter][sectorcounter]]
                 
-"""
-TO DO
-get fieldcheck working
-if int can be used put it into the big matrix
-what if the whole range is not usable and backtracking has to be used
-"""
+
+        
+def remember(row,column,minejums):
+    global memoryrow
+    global memorycol
+    global memorycell
+    memoryrow = row
+    memorycol = column
+    memorycell = minejums
+        
+def mistakechecker(number,row,column):
+    if rowcheck(number,row) == False and columncheck(number,column) == False and fieldcheck(number,row,column) == False:
+        return True
+    return False
+
+def guessingalgo(row,col,cellix):
+    
+    global previous
+    if cellix == 0:
+        previous = 1
+    else:
+        previous = cellix
+    for guess in range(previous,10):
+        if mistakechecker(guess,row,col):
+            remember(row,col,guess)
+            newmatrix(guess,row,col)
+            print(rows)
+            print("")            
+        if rows[row][col] == guess:
+            break
+        if guess == 9 and rows[row][col] != 9:
+            print("zhopix")
+            if cellix == 0:
+                previous = 1
+            else:
+                previous = memorycell
+            sys.exit() 
+            for guess in range(previous,10):
+                 
+                if mistakechecker(guess,row,col):
+                    remember(row,col,cellix)
+                    newmatrix(guess,row,col)
+                    print(rows)
+                    print("")
+    
+                if rows[row][col] == guess:
+                    break
+                if guess == 9 and rows[row][col] != 9:
+                    print("zhopix")
+                    sys.exit()
+
+
+
+            
+
+
+
+rowcounter = -1
+columncounter = -1                 
+for selectedrow in rows:
+    rowcounter += 1
+
+    for selectedcell in selectedrow:
+        columncounter += 1
+            
+        if selectedcell == 0:
+            guessingalgo(rowcounter, columncounter,selectedcell)
+            
+    columncounter = -1
+
+
                 
+  
+                
+   
