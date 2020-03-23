04cc06419fedf12e3e5641622d4a0452a2e5fcaf
rexety <andozelto@gmail.com>
2020-03-23 15:38:01 +0100

    asd

--- strategy.py
+++ strategy.py
@@ -3,8 +3,8 @@ import numpy as np
 import sys
 # load given sudoku field and rename it to rows
 rows = np.load('example.npy')
-memorycell = 0
-previous = 1
+
+
 # manipulate given matrix so that columns are in rows
 columns = np.rot90(rows)
 columns = np.flip(columns, 0)
@@ -20,6 +20,22 @@ for x in range(0,9,3):
          tempfield = np.append(tempfield,rows[x:y, i:temp])
                                                          
 fields = np.reshape(tempfield,(9,9))
+
+
+
+print(rows)
+print(columns)
+print(fields)
+
+
+rows[1][1] = 9
+rows[7][7] = 7
+
+print(rows)
+print(columns)
+print(fields)
+
+
 
 
 
@@ -76,39 +92,50 @@ def fieldcheck(number,row,column):
                 if column < j:
                     return number in fields[sectormatrix[counter][sectorcounter]]
                 
-
-        
+"""
+                
+depth = 0
+memoryrow = [-1]
+memorycol = [-1]
+memorycell = [-1]
 def remember(row,column,minejums):
     global memoryrow
     global memorycol
     global memorycell
-    memoryrow = row
-    memorycol = column
-    memorycell = minejums
-        
+    global depth
+    depth = 0
+    memoryrow.append(row)
+    memorycol.append(column)
+    memorycell.append(minejums)
+"""        
 def mistakechecker(number,row,column):
     if rowcheck(number,row) == False and columncheck(number,column) == False and fieldcheck(number,row,column) == False:
         return True
     return False
-
+"""
 def guessingalgo(row,col):
+    global depth
     global rows
+    if counter == 0:
+        sys.exit()
     for guess in range(1,10):
-        if guess == memorycell:
+        if guess == memorycell[-1-depth]:
             continue
         if mistakechecker(guess,row,col):
             remember(row,col,guess)
             newmatrix(guess,row,col)
             print(rows)
-            print("")            
+            print("")
+            depth = 0
+            
         if rows[row][col] == guess:
-            atmina = 0
             break
         if guess == 9 and rows[row][col] != 9:
-            rows[memoryrow][memorycol] = 0
-            print(rows)
-            return False
-"""
+            depth += 1
+            rows[memoryrow[-1-depth]][memorycol[-1-depth]] = 0
+            solve(rows)
+
+
 
 def solve(rows):
     rowcounter = -1
@@ -126,26 +153,46 @@ def solve(rows):
             
 solve(rows)
 
-"""
-counter = 4
+counter = 500
 
-
-rowcounter = -1
-columncounter = -1                 
-for selectedrow in rows:
-    rowcounter += 1
-
-    for selectedcell in selectedrow:
-        columncounter += 1
-            
-        if selectedcell == 0:
-            if guessingalgo(rowcounter, columncounter) == False:
-                sys.exit()
-                break
-
-            
-    columncounter = -1
-
-
+def solve(rowX):
+    
+    global rowcounter
+    global columncounter
+    global counter
+    counter -= 1
+    if counter == 0:
+        sys.exit()
+    rowcounter = -1
+    columncounter = -1                 
+    for selectedrow in rowX:
+        rowcounter += 1
+    
+        for selectedcell in selectedrow:
+            columncounter += 1
                 
-   
+            if selectedcell == 0:
+                guessingalgo(rowcounter, columncounter)
+    
+    
+                
+        columncounter = -1
+
+solve(rows)
+
+"""
+def searchzero(matrix):
+    rowloc = -1
+    colloc = -1
+    for rows in matrix:
+        rowloc += 1
+        for cell in rows:
+            colloc += 1
+            if cell == 0:
+                return  rowloc,colloc
+            
+            
+            
+            
+#def guessing(row,col):
+    
